b17b8a0caf2aa43702a87d754e612c7a5f8479ef
Sodiq Taiwo <alhassansahih@gmail.com>
2022-11-02 00:02:47 +0100

    setting up home page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // styles
 import './Profile.css'
 
-export default function Profile({ data, totalPages, page, setPage, isPending }) {
+export default function Profile() {
   const [hover, setHover] = useState(false)
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
 
-  if(data.length === 0) {
+  const [users, setUsers] = useState([])
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  const url = `https://api.github.com/search/users?q=lagos&page=${page}`
+
+  useEffect(() => {
+    const getUser = async () => {
+
+      setIsPending(true)
+
+      try {
+        const res = await fetch(url)
+
+        if(!res.ok) {
+          throw new Error(res.statusText)
+        }
+
+        const data = await res.json()
+
+        setError(false)
+        setIsPending(false)
+        setUsers([...users, ...data.items])
+        // setUsers(data.items)
+        setTotalPages(Math.round(data.total_count / 30))
+        
+      }
+      catch(err) {
+        setIsPending(false)
+        console.log(err.message)
+        setError(err.message)
+      }
+    }
+    getUser()
+  }, [page, url])
+
+  if(users.length === 0) {
     return <div className='error'>No user profile...</div> 
   }
 
   return (
     <div className='container'>
       <div className="profile">
-        {data.map(profile => (
+        {users.map(profile => (
           <div
             className='preview' 
             key={profile.id}
