c232795c99c2e3e2dc39f7f454a3073d7860463d
Sodiq Taiwo <alhassansahih@gmail.com>
2022-11-04 13:47:21 +0100

    fetching repository

--- src/pages/repository/Repository.js
+++ src/pages/repository/Repository.js
@@ -1,14 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 // static files  
 import './Repository.css'
 import repo from '../../icons/repository.png'
+import star from '../../icons/star.png'
+import fork from '../../icons/fork.png'
 import image from '../../img/logo.JPG'
 
 export default function Repository () {
     const {id} = useParams()
+    const [repositories, setRepositories] = useState([])
+
     useEffect(() => {
-        // fetchRepo()
+        fetchRepo()
     }, [])
     const fetchRepo = async () => {
        const url =  'https://api.github.com/users/'+ id +'/repos';
@@ -16,6 +20,7 @@ export default function Repository () {
         const res = await fetch(url);
         const data = await res.json();
         console.log(data)
+        setRepositories(data)
        }
        catch(err) {
         console.log(err.message)
@@ -25,24 +30,32 @@ export default function Repository () {
     return ( 
         <div className="repository">
           <div className='banner'></div>
-          <img className='owner-img' src={image} />
+          <img className='owner-img' src={'https://avatars.githubusercontent.com/u/8760121?v=4'} />
           <div className='owner'>
-            <h2>Hassan Warwick</h2>
+            <h2>{ id }</h2>
           </div>
           <div className='repo'>
             <h4>Repositories</h4>
             <ul className='repo-main-list'>
-                <li className='repo-main-list-li'>
+            {repositories && repositories.map(repository => (
+                <li className='repo-main-list-li' key={repository.archive_url}>
                     <div>
-                        <img className='repo-icon' src={repo} />
-                        <p>ecommerce website</p>
+                        <img className='repo-icon' src={repo} alt='repo-icon'/>
+                        <p>{ repository.name }</p>
                     </div>
                     <ul className='repo-sub-list'>
-                        <li>javaScript</li>
-                        <li>3</li>
-                        <li>3</li>
+                        <li>{ repository.language }</li>
+                        <li>
+                            <img className='repo-icon' src={star} alt='star-icon' />
+                            <span>{ repository.stargazers_count }</span>
+                        </li>
+                        <li>
+                            <img className='repo-icon' src={fork} alt='fork-icon' />
+                            <span>{ repository.star }</span>
+                        </li>
                     </ul>
                 </li>
+            ))}
             </ul>
           </div>
         </div>
